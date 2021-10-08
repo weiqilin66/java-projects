6438388556617e8f65f680d33ef763c99ab30d3b
linwq1 <linwq1@yusys.com.cn>
2021-10-08 14:26:13 +0800

    1
    Change-Id: I86d233b732de68b0ee7ecd93ebce739eab662612

--- projects/card-server/src/main/java/org/wayne/card/CardServer.java
+++ projects/card-server/src/main/java/org/wayne/card/CardServer.java
@@ -1,7 +1,9 @@
-package org.wayne;
+package org.wayne.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.wayne.common.init.HtConfigSetting;
 
 /**
@@ -9,6 +11,8 @@ import org.wayne.common.init.HtConfigSetting;
  * @author: lwq
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class CardServer {
     public static void main(String[] args) {
         new HtConfigSetting();
